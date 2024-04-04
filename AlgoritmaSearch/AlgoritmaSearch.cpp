b1f25d86e259fd1c32078f784b4ca85faea818a0
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-04-04 11:07:55 +0700

    membuat prosedur display

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -39,7 +39,33 @@ void LinearSearch()
 		//accept the number to be searched
 		cout << "\nEnter the element you want to search: "; //step 1
 		cin >> item;
+
+		ctr = 0;
+		i = 0; //step 2
+		while (i < n) //step 3
+		{
+			ctr++;
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl; //step 6
+				break;
+			}
+			i++; //step 4
+		}
+
+		if (i == n) // step 5
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue seacrh (y/n): ";
+		cin >> ch;
+
 	} while ((ch == 'y') || (ch == 'Y'));
+}
+
+void display()
+{
+
 }
 
 int main() {
